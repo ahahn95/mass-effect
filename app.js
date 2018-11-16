468d49a43b67a26a7ad0867af6db45960ced3005
Alexander Hahn <ahahn95@gmail.com>
2018-11-16 15:01:02 -0500

    allow cors

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-
+const cors = require('cors');
 var app = express();
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
